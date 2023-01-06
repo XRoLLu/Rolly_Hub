d562f629f520608f79dea6e49b79ba7483c8128d
XRoLLu <106608192+XRoLLu@users.noreply.github.com>
2023-01-06 21:45:00 +0700

    Create MainLoadstring.lua

---
+++ MainLoadstring.lua
@@ -0,0 +1,8 @@
+--Main Loadstring
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XRoLLu/Rolly_Hub/main/open-source-trash-loader.exe.yeah"))()
+setclipboard("https://discord.gg/dWUdqZUvNB")
+
+
+--please join the discord server to receive new scripts and update!
+--the discord link will be set in your clipboard when you execute the scripts
